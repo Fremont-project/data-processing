3e58866dafcf2d432ac3270fac90463a0edfa0f7
Michal Takac <takacmichal92@gmail.com>
2020-03-05 17:34:12 -0800

    running demand pipeline on new internal TAZs

--- pipeline/demand/demandutils.py
+++ pipeline/demand/demandutils.py
@@ -4,6 +4,24 @@ from geovoronoi import coords_to_points, points_to_coords, voronoi_regions_from_
 from geovoronoi.plotting import subplot_for_map, plot_voronoi_polys_with_points_in_area
 
 
+def to_gdf(path):
+    import pandas as pd
+    import geopandas as gpd
+    from shapely import wkt
+
+    if (path.endswith('.shp')):
+        gdf = GeoDataFrame.from_file(path)
+        gdf = gdf.to_crs('epsg:4326')
+        return gdf
+    elif (path.endswith('.csv')):
+        # https://geopandas.readthedocs.io/en/latest/gallery/create_geopandas_from_pandas.html#from-wkt-format
+        df = pd.read_csv(path)
+        df['geometry'] = df['geometry'].apply(wkt.loads)
+        gdf = gpd.GeoDataFrame(df)
+        gdf.crs = 'epsg:4326'
+        return gdf
+
+
 def add_point_geometry(input_path, output_path, lng_column='lng', lat_column='lat', geometry_column='geometry', crs='epsg:4326'):
     """
     Add a new Point geometry column
@@ -34,19 +52,18 @@ def add_point_geometry(input_path, output_path, lng_column='lng', lat_column='la
     gdf = gdf.rename(columns={'geometry': geometry_column}
                      ).set_geometry(geometry_column)
     gdf = gdf[['leg_id', 'start_time', geometry_column]]
-    gdf.to_file(output_path)
+    gdf.to_csv(output_path)
 
 
 def neighborhoods_touching_internal_legs(neighborhoods_path, points_gdf_path, output_path):
+    import pandas as pd
     import geopandas as gpd
 
-    neighborhoods_shp = GeoDataFrame.from_file(neighborhoods_path)
-    neighborhoods_shp = neighborhoods_shp.to_crs('epsg:4326')
+    neighborhoods_shp = to_gdf(neighborhoods_path)
 
     assert(neighborhoods_shp.crs == 'epsg:4326'),'Neighborhoods shapefile Coordinate Reference System missmatch! CRS is not EPSG:4326.'
 
-    points_gdf = gpd.GeoDataFrame.from_file(points_gdf_path)
-    points_gdf = points_gdf.to_crs('epsg:4326')
+    points_gdf = to_gdf(points_gdf_path)
 
     int_neighborhood_names = _get_internal_neighborhoods(points_gdf, neighborhoods_shp)
 
@@ -57,7 +74,7 @@ def neighborhoods_touching_internal_legs(neighborhoods_path, points_gdf_path, ou
         int_neighborhoods = int_neighborhoods.append(particular_neighborhood, ignore_index=True)
 
     print('Filtered {x} neighborhoods.'.format(x=repr(len(int_neighborhoods['NAME'].unique()))))
-    int_neighborhoods.to_file(output_path)
+    int_neighborhoods.to_csv(output_path)
 
 
 def geovor_in_neighborhoods_random_points(neighborhoods_path, points_gdf_path, output_path):
@@ -66,13 +83,11 @@ def geovor_in_neighborhoods_random_points(neighborhoods_path, points_gdf_path, o
 
     np.random.seed(123)
 
-    neighborhoods_shp = GeoDataFrame.from_file(neighborhoods_path)
-    neighborhoods_shp = neighborhoods_shp.to_crs('epsg:4326')
+    neighborhoods_shp = to_gdf(neighborhoods_path)
 
     assert(neighborhoods_shp.crs == 'epsg:4326'),'Neighborhoods shapefile Coordinate Reference System missmatch! CRS is not EPSG:4326.'
 
-    nodes = gpd.GeoDataFrame.from_file(points_gdf_path)
-    nodes = nodes.to_crs('epsg:4326')
+    nodes = to_gdf(points_gdf_path)
 
     int_neighborhoods = _get_internal_neighborhoods(nodes, neighborhoods_shp)
 
@@ -98,13 +113,11 @@ def geovor_in_neighborhoods_random_points(neighborhoods_path, points_gdf_path, o
 def geovor_in_neighborhoods_real_points(neighborhoods_path, points_gdf_path, output_path):
     import geopandas as gpd
 
-    neighborhoods_shp = GeoDataFrame.from_file(neighborhoods_path)
-    neighborhoods_shp = neighborhoods_shp.to_crs('epsg:4326')
+    neighborhoods_shp = to_gdf(neighborhoods_path)
 
     assert(neighborhoods_shp.crs == 'epsg:4326'),'Neighborhoods shapefile Coordinate Reference System missmatch! CRS is not EPSG:4326.'
 
-    points_gdf = gpd.GeoDataFrame.from_file(points_gdf_path)
-    points_gdf = points_gdf.to_crs('epsg:4326')
+    points_gdf = to_gdf(points_gdf_path)
 
     int_neighborhoods = _get_internal_neighborhoods(points_gdf, neighborhoods_shp)
 
@@ -130,18 +143,189 @@ def geovor_in_neighborhoods_real_points(neighborhoods_path, points_gdf_path, out
 
 def visual_check_internal_taz(neighborhoods_path, points_path):
     from keplergl import KeplerGl
-    import pandas as pd
     import geopandas as gpd
 
-    nodes = gpd.GeoDataFrame.from_file(points_path)
-    nodes = nodes.to_crs('epsg:4326')
-
-    internal_neighborhoods = pd.read_csv(neighborhoods_path)
+    nodes = to_gdf(points_path)
+    taz = to_gdf(neighborhoods_path)
 
     fremont_map = KeplerGl(height=600)
     fremont_map.add_data(data=nodes, name="Nodes")
-    fremont_map.add_data(data=internal_neighborhoods, name="Fremont neighborhoods")
+    fremont_map.add_data(data=taz, name="Fremont neighborhoods")
     return fremont_map
+
+
+def visual_check_OD(od_path, taz_path, html_output, label="OD matrix"):
+    from keplergl import KeplerGl
+    map_1 = KeplerGl(height=600)
+    map_1.add_data(data=to_gdf(od_path), name=label)
+    map_1.add_data(data=to_gdf(taz_path), name="Internal TAZs")
+
+    config = {
+        'version': 'v1',
+        'config': {
+            'mapState': {
+                'latitude': 37.514170259568004,
+                'longitude': -121.94452016977094,
+                'zoom': 11.332206958925893
+            }
+        }
+    }
+    map_1.config = config
+    map_1.save_to_html(file_name=html_output, config=config)
+    return map_1
+
+def visual_check_OD_both_taz(od_path, int_taz_path, ext_taz_path, html_output, label="OD matrix"):
+    from keplergl import KeplerGl
+    map_1 = KeplerGl(height=600)
+    map_1.add_data(data=to_gdf(od_path), name=label)
+    map_1.add_data(data=to_gdf(int_taz_path), name="Internal TAZs")
+    map_1.add_data(data=to_gdf(ext_taz_path), name="External TAZs")
+
+    config = {
+        'version': 'v1',
+        'config': {
+            'mapState': {
+                'latitude': 37.514170259568004,
+                'longitude': -121.94452016977094,
+                'zoom': 11.332206958925893
+            }
+        }
+    }
+    map_1.config = config
+    map_1.save_to_html(file_name=html_output, config=config)
+    return map_1
+
+
+def sjoin_int_int(start_nodes_path, end_nodes_path, taz_path, output_path):
+    from geopandas import GeoDataFrame
+
+    taz = to_gdf(taz_path)
+    start_nodes = to_gdf(start_nodes_path)
+    end_nodes = to_gdf(end_nodes_path)
+
+    df1 = sjoin(taz, start_nodes, how='right', op='contains')
+    df1.rename(
+        columns={
+            "CentroidID": "CentroidID_O"
+        },
+        inplace=True
+    )
+    print(df1)
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df1.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+
+    df2 = sjoin(taz, end_nodes, how='right', op='contains')
+    df2.rename(
+        columns={
+            "CentroidID": "CentroidID_D"
+        },
+        inplace=True
+    )
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df2.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+    od_matrix = df1.combine_first(df2)
+    od_matrix['OBJECTID'] = od_matrix.index + 1
+    #od_matrix = od_matrix[['CentroidID_O','CentroidID_D','OBJECTID']]
+    od_matrix.to_csv(output_path)
+
+def sjoin_int_ext(start_nodes_path, end_nodes_path, int_taz_path, ext_taz_path, output_path):
+    from geopandas import GeoDataFrame
+
+    int_taz = to_gdf(int_taz_path)
+    ext_taz = to_gdf(ext_taz_path)
+    ext_taz.drop('OBJECTID', axis=1, inplace=True)
+    start_nodes = to_gdf(start_nodes_path)
+    end_nodes = to_gdf(end_nodes_path)
+
+    df1 = sjoin(int_taz, start_nodes, how='right', op='contains')
+    df1.rename(
+        columns={
+            "CentroidID": "CentroidID_O"
+        },
+        inplace=True
+    )
+    print(df1)
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df1.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+
+    df2 = sjoin(ext_taz, end_nodes, how='right', op='contains')
+    df2.rename(
+        columns={
+            "CentroidID": "CentroidID_D"
+        },
+        inplace=True
+    )
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df2.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+    od_matrix = df1.combine_first(df2)
+    od_matrix['OBJECTID'] = od_matrix.index + 1
+    #od_matrix = od_matrix[['CentroidID_O','CentroidID_D','OBJECTID']]
+    od_matrix.to_csv(output_path)
+
+
+def sjoin_ext_int(start_nodes_path, end_nodes_path, ext_taz_path, int_taz_path, output_path):
+    from geopandas import GeoDataFrame
+
+    ext_taz = to_gdf(ext_taz_path)
+    ext_taz.drop('OBJECTID', axis=1, inplace=True)
+    int_taz = to_gdf(int_taz_path)
+    start_nodes = to_gdf(start_nodes_path)
+    end_nodes = to_gdf(end_nodes_path)
+
+    df1 = sjoin(ext_taz, start_nodes, how='right', op='contains')
+    df1.rename(
+        columns={
+            "CentroidID": "CentroidID_O"
+        },
+        inplace=True
+    )
+    print(df1)
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df1.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+
+    df2 = sjoin(int_taz, end_nodes, how='right', op='contains')
+    df2.rename(
+        columns={
+            "CentroidID": "CentroidID_D"
+        },
+        inplace=True
+    )
+    for column in ['index_left', 'index_right', 'OBJECTID']:
+        try:
+            df2.drop(column, axis=1, inplace=True)
+        except KeyError:
+            # ignore if there are no index columns
+            pass
+
+    od_matrix = df1.combine_first(df2)
+    od_matrix['OBJECTID'] = od_matrix.index + 1
+    #od_matrix = od_matrix[['CentroidID_O','CentroidID_D','OBJECTID']]
+    od_matrix.to_csv(output_path)
 
 #
 # Internal functions
@@ -157,30 +341,6 @@ def _select_gdf_by_geometry(gdf, position='origin', start_node_geometry_column='
     if position not in ['origin', 'destination']:
         raise ValueError(
             '{type} argument is incorrect, use "origin" or "destination"'.format(type=repr(position)))
-
-    return gdf_to_join
-
-
-def _spatial_join_nodes_with_centroids(gdf, centroid_zones, how='left', op='within', position='origin', start_node_geometry_column='start_node_geometry', end_node_geometry_column='end_node_geometry'):
-    gdf_to_join = _select_gdf_by_geometry(
-        gdf, position, start_node_geometry_column, end_node_geometry_column)
-
-    centroid_id_column = "CentroidID_O" if type == 'origin' else "CentroidID_D"
-
-    gdf_to_join = sjoin(gdf_to_join, centroid_zones, how='left', op='within')
-    gdf_to_join.rename(
-        columns={
-            "CentroidID": centroid_id_column
-        },
-        inplace=True
-    )
-
-    for column in ['index_left', 'index_right', 'OBJECTID']:
-        try:
-            gdf_to_join.drop(column, axis=1, inplace=True)
-        except KeyError:
-            # ignore if there are no index columns
-            pass
 
     return gdf_to_join
 
